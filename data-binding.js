1ff344890124d26bb175a3b92c9e11459dee171d
Pat Cavit <pcavit@gmail.com>
2014-07-27 15:47:02 -0700

    No longer an extension, now just generic library
    Simpler to reason about this way, and could easily be used within an
    extension (which I plan to do).

--- data-binding.js
+++ data-binding.js
@@ -1,74 +1,49 @@
-YUI.add("extension-data-binding", function(Y) {
+YUI.add("data-binding", function(Y) {
 
-    var DataBinding = function() {};
+    var DataBinding;
     
-    DataBinding.ATTR2EVENT = {
-        value : "input"
+    DataBinding = function(args) {
+        this._root = Y.one(args.root);
+        this._source = args.source;
+        
+        this._handles = {
+            global : [],
+            source : [],
+            dom    : []
+        };
+        
+        this._bindings = {
+            events : {},
+            attrs  : {}
+        };
+        
+        // Since binding is the default we only skip it if args.bind is specified & falsey.
+        if("bind" in args && !args.bind) {
+            return;
+        }
+        
+        this.bind();
     };
     
-    DataBinding.EVENT2ATTR = {
-        input : "value"
-    };
-
     DataBinding.prototype = {
-        initializer : function(config) {
-            this._dbHandles = {
-                global : [],
-                source : [],
-                dom    : []
-            };
-            
-            this._dbBindings = {
-                events : {},
-                attrs  : {}
-            };
-            
-            this._dbHandles.global.push(
-                Y.Do.after(this._afterRender, this, "render", this)
-            );
+        MAPPINGS : {
+            value : "input",
+            input : "value"
         },
-
+        
         destructor : function() {
-            Y.Object.each(this._dbHandles, function detachHandle(handles) {
+            Y.Object.each(this._handles, function detachHandle(handles) {
                 new EventTarget(handles).detach();
             });
             
-            this._dbHandles = null;
+            this._handles = this._bindings = null;
         },
 
-        _afterRender : function() {
-            var _this     = this,
-                container = this.get("container"),
-                nodes     = container.all("[data-bound]"),
-                handles   = this._dbHandles;
-            
-            if(!this._dbSource) {
-                this.bindingSource(this);
-            }
-            
-            nodes.each(this._parseNode, this);
-            
-            Y.Object.each(this._dbBindings.events, function(listeners, event) {
-                listeners.forEach(function(listener) {
-                    handles.dom.push(
-                        Y.delegate(event, _this["_" + event + "Event"], container, listener.id, _this, listener.attr)
-                    );
-                });
-            });
-            
-            Y.Object.each(this._dbBindings.attrs, function(listeners, attr) {
-                handles.source.push(
-                    _this._dbSource.on(attr + "Change", _this._attrChange, _this, listeners)
-                );
-            });
-        },
-        
         _parseNode : function(node) {
             var _this     = this,
-                container = this.get("container"),
                 id        = "#" + node.generateID(),
                 config    = node.getData("bound"),
-                bindings  = this._dbBindings;
+                bindings  = this._bindings;
                 
             if(!config) {
                 return;
@@ -78,28 +53,28 @@ YUI.add("extension-data-binding", function(Y) {
                 var parts = binding.split(":"),
                     type   = parts[0],
                     attr  = parts[1],
-                    event = DataBinding.ATTR2EVENT[type];
+                    event = _this.MAPPINGS[type];
                 
                 console.log("%o has binding %s <=> %s", node.getDOMNode(), type, attr);
                 
                 // This work only happens if the dom attribute can be mapped to a dom event
                 if(event) {
-                    if(!bindings.events[event]) {
-                        bindings.events[event] = [];
+                    if(!_this._bindings.events[event]) {
+                        _this._bindings.events[event] = [];
                     }
                     
-                    bindings.events[event].push({
+                    _this._bindings.events[event].push({
                         id   : id,
                         attr : attr
                     });
                 }
                 
                 // The binding from YUI Attribute -> dom attribute always happens
-                if(!bindings.attrs[attr]) {
-                    bindings.attrs[attr] = [];
+                if(!_this._bindings.attrs[attr]) {
+                    _this._bindings.attrs[attr] = [];
                 }
                 
-                bindings.attrs[attr].push({
+                _this._bindings.attrs[attr].push({
                     id   : id,
                     type : type
                 });
@@ -107,9 +82,9 @@ YUI.add("extension-data-binding", function(Y) {
         },
 
         _inputEvent : function(e, attr) {
-            this.set(
+            this._source.set(
                 attr,
-                e.target.get(DataBinding.EVENT2ATTR[e.type]),
+                e.target.get(this.MAPPINGS[e.type]),
                 {
                     source : "#" + e.target.generateID(),
                     type   : e.type
@@ -118,14 +93,14 @@ YUI.add("extension-data-binding", function(Y) {
         },
         
         _attrChange : function(e, listeners) {
-            var container = this.get("container");
+            var root = this._root;
             
             listeners.forEach(function(listener) {
                 if(e.source === listener.id && e.type === listener.type) {
                     return;
                 }
                 
-                var node = container.one(listener.id),
+                var node = root.one(listener.id),
                     type = listener.type;
                 
                 if(type === "value" || type === "text") {
@@ -141,18 +116,32 @@ YUI.add("extension-data-binding", function(Y) {
         },
 
         // Public API
-        bindingSource : function(source) {
-            if(this._dbSource) {
-                // TODO: unbind from existing source
-            }
+        bind : function() {
+            var _this   = this,
+                nodes   = this._root.all("[data-bound]"),
+                handles = this._handles;
             
-            this._dbSource = source;
+            nodes.each(this._parseNode, this);
             
-            // TODO: bind to new source
+            console.log(this._bindings);
+            
+            Y.Object.each(this._bindings.events, function(listeners, event) {
+                listeners.forEach(function(listener) {
+                    handles.dom.push(
+                        Y.delegate(event, _this["_" + event + "Event"], _this._root, listener.id, _this, listener.attr)
+                    );
+                });
+            });
+            
+            Y.Object.each(this._bindings.attrs, function(listeners, attr) {
+                handles.source.push(
+                    _this._source.on(attr + "Change", _this._attrChange, _this, listeners)
+                );
+            });
         }
     };
 
-    Y.namespace("Extensions").DataBinding = DataBinding;
+    Y.DataBinding = DataBinding;
 
 }, "@VERSION@", {
     requires : [
